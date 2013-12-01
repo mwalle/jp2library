c3c5c1962d5e2576061ba6ad0b39731c249d1039
Michael Walle <michael@walle.cc>
2013-12-01 23:38:10 +0100

    jp2cli: add read command and getopt support

--- tools/jp2cli.c
+++ tools/jp2cli.c
@@ -21,14 +21,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <errno.h>
 #include <assert.h>
+
 #include "jp2library.h"
 
 static struct jp2_remote *r;
+static const char *prog;
 
-void usage(const char *prog)
+void usage()
 {
-	printf("usage: %s <command> [<args>..]\n", prog);
+	printf(
+		"usage: %s <options> <command> ..\n"
+		"\n"
+		"Available options:\n"
+		"\t-D dev  Specify device to use. Default is /dev/ttyUSB0.\n"
+		"\t-h      Print this help.\n"
+		"\t-v      Be more verbose.\n"
+		"\n"
+		"Available commands:\n"
+		"\tinfo\n"
+		"\t        Print information about detected remote.\n"
+		"\tread <outfile> <start> <length>\n"
+		"\t        Read <length> bytes from remote at offset <start>.\n"
+		"\terase <start> <length>\n"
+		"\t        Erase the given area. Please not that only whole\n"
+		"\t        erase blocks can be erased.\n"
+		"\twrite <infile> <start> <length>\n"
+		"\t        Write <length> bytes from remote to offset <start>.\n"
+		"\traw [bytes..]\n"
+		"\t        Send an raw command to the remote.\n"
+		, prog);
 }
 
 static int cmd_info(int argc, char **argv)
@@ -53,6 +77,72 @@ static int cmd_info(int argc, char **argv)
 	return 0;
 }
 
+static int cmd_read(int argc, char **argv)
+{
+	int rc;
+	FILE *f;
+	int address;
+	int length;
+	char *endptr;
+
+	if (argc != 4) {
+		usage();
+		return EXIT_FAILURE;
+	}
+
+	address = strtoul(argv[2], &endptr, 0);
+	if (*argv[2] != '\0' && *endptr != '\0') {
+		printf("could not parse start address\n");
+		return -1;
+	}
+
+	length = strtoul(argv[3], &endptr, 0);
+	if (*argv[3] != '\0' && *endptr != '\0') {
+		printf("could not parse length\n");
+		return -1;
+	}
+
+	f = fopen(argv[1], "wb");
+	if (!f) {
+		printf("could not open %s: %s", argv[1], strerror(errno));
+		return -1;
+	}
+
+	while (length > 0) {
+		uint8_t buf[128];
+
+		printf("Reading %05Xh\n", address);
+		rc = jp2_read_block(r, address, sizeof(buf), buf);
+		if (rc < 0) {
+			printf("could not read from remote\n");
+			break;
+		}
+
+		length -= rc;
+		address += rc;
+
+		rc = fwrite(buf, rc, 1, f);
+		if (rc < 0) {
+			printf("could not write to file\n");
+			break;
+		}
+	}
+
+	fclose(f);
+
+	return rc;
+}
+
+static int cmd_erase(int argc, char **argv)
+{
+	return 0;
+}
+
+static int cmd_write(int argc, char **argv)
+{
+	return 0;
+}
+
 static int cmd_raw(int argc, char **argv)
 {
 	uint8_t cmd[16];
@@ -70,24 +160,51 @@ static int cmd_raw(int argc, char **argv)
 int main(int argc, char **argv)
 {
 	int rc;
+	int opt;
 	struct jp2_info info;
+	const char *dev = "/dev/ttyUSB0";
+
+	prog = argv[0];
+
+	while ((opt = getopt(argc, argv, "D:hv")) != -1) {
+		switch (opt) {
+		case 'D':
+			dev = optarg;
+			break;
+		case 'h':
+			usage();
+			exit(EXIT_SUCCESS);
+		case 'v':
+			setenv("JP2_DEBUG", "1", 1);
+			break;
+		default:
+			usage();
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (optind >= argc) {
+		usage();
+		exit(EXIT_FAILURE);
+	}
 
 	jp2_init();
-	r = jp2_open_remote("/dev/ttyUSB0");
+	r = jp2_open_remote(dev);
 
 	jp2_enter_loader(r);
 
 	jp2_get_info(r, &info);
 
-	if (argc < 2) {
-		usage(argv[0]);
-		return -1;
-	}
-
-	if (!strcmp(argv[1], "info")) {
-		rc = cmd_info(argc, argv);
-	} else if (!strcmp(argv[1], "raw")) {
-		rc = cmd_raw(argc, argv);
+	if (!strcmp(argv[optind], "info")) {
+		rc = cmd_info(argc - optind, argv + optind);
+	} else if (!strcmp(argv[optind], "read")) {
+		rc = cmd_read(argc - optind, argv + optind);
+	} else if (!strcmp(argv[optind], "erase")) {
+		rc = cmd_erase(argc - optind, argv + optind);
+	} else if (!strcmp(argv[optind], "write")) {
+		rc = cmd_write(argc - optind, argv + optind);
+	} else if (!strcmp(argv[optind], "raw")) {
+		rc = cmd_raw(argc - optind, argv + optind);
 	}
 
 	jp2_exit_loader(r);
